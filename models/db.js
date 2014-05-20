35bc41b911d571e795738c7e3daeeb91c5f3fdc6
mrc <313187850@qq.com>
2014-05-20 23:26:24 +0800

    2014年05月20日23:26:23
    Signed-off-by: mrc <313187850@qq.com>

--- models/db.js
+++ models/db.js
@@ -1,5 +1,21 @@
-var settings = require('../settings'),
-    Db = require('mongodb').Db,
-    Connection = require('mongodb').Connection,
-    Server = require('mongodb').Server;
-module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT), {safe: true});
+// var settings = require('../settings'),
+//     Db = require('mongodb').Db,
+//     Connection = require('mongodb').Connection,
+//     Server = require('mongodb').Server;
+// module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT), {safe: true});
+// ***********************************
+// -----------------------------------
+var mongodb = require('mongodb');
+
+var dbServer = new mongodb.Server(process.env.OPENSHIFT_MONGODB_DB_HOST,parseInt(process.env.OPENSHIFT_MONGODB_DB_PORT)),
+	db = new mongodb.Db('v2push', dbServer, {auto_reconnect: true}),
+	dbUser = process.env.OPENSHIFT_MONGODB_DB_USERNAME,
+	dbPass = process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
+	ipaddr  = process.env.OPENSHIFT_NODEJS_IP;
+  	port    = parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8080;
+
+module.exports = db;
+
+
+
+
